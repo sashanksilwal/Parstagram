0cb4e8d26848075784dbf08f4dc0e4008bc2fffe
sashanksilwal <sashanksilwal@hotmail.com>
2021-10-16 21:19:25 -0400

    adding basics unit 5

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,11 +6,23 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+   
+    
 
-class FeedViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -25,5 +37,41 @@ class FeedViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts,error) in
+            if posts != nil{
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        
+        cell.captionLabel.text = post["caption"] as! String
+        cell.userNameLabel.text = user.username
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 
 }
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Sashank Silwal on 10/16/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+   
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
